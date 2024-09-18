1e2b86bf3f8e1922ff3fabea451ce921f0dc611f
ostrowerkhov <ostrowerkhov@icloud.com>
2024-09-18 17:32:36 +0300

    Initial commit

---
+++ src/main/java/ru/ostrowerkhov/VacationPayCalcApplication.java
@@ -0,0 +1,11 @@
+package ru.ostrowerkhov;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VacationPayCalcApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(VacationPayCalcApplication.class, args);
+    }
+}
